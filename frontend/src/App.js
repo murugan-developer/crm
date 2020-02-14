8552bb846a966882dd19aaac97f3247fb5c57a65
Murugan R <murugan.r@domain.rage-india.com>
2020-02-14 17:51:58 +0530

    install boostrap frame work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Accordion, Card, Header, Collapse, Toggle, Body, Button } from 'react-bootstrap';
+
+
 function App() {
+
+  const numbers = [1, 2, 3, 4, 5];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          numbers.map((number) => 
+          <li>{number}</li>
+          )
+        }
     </div>
   );
 }
